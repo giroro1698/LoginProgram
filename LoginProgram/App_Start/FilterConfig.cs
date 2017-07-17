6507284acba58a045eb5d73781479fcce10d50b2
SongK <SongK@1611004NB01>
2017-07-17 19:50:31 +0800

    加入專案檔案。

---
+++ LoginProgram/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LoginProgram
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
